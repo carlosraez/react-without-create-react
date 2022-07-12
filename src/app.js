c25d3fa8330a74e41bb24615063ab646aac1892d
Carlos Raez <carlosraez@gmail.com>
2022-07-12 21:18:53 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+ const App = () => {
+  return (
+    <div className="app">
+    <h1>Mi primera App</h1>
+    <p>Este es un ejemplo de una app sin <span>create-react-app</span></p>
+    </div>
+  )
+}
+
+export default App
+
